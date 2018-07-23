f7cdda793c21b961bd2f7af1331a1679bee49f27
Varun <varunm0503@gmail.com>
2018-07-23 23:08:17 +0530

    First commit

---
+++ src/AppInfoSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class AppInfoSection extends React.Component {
+	render(){
+		return (
+			<div>
+			<div className="app-info-section">
+				WE UNEARTH DESIGN
+			</div>
+			<div className="app-info__who-we-are">
+				<div className="app-info__who-we-are-header">
+					<span className="app-info__who-we-are-span"> WHO WE ARE </span>
+				</div>
+				<div className="app-info-section__info-detail">
+					Strota is a consultant company having more than 9 years of experience in the field of Vastu,
+					Astrology & Astrological Vastu. Lalit Nehra a marketing professional and Mohit Wadhwa , an
+					interior designer partnered with the desire to share their knowledge in the field .
+				</div>
+			</div>
+			</div>
+		);
+	}
+}
+
+export default AppInfoSection;
